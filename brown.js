e3642cae1ee0638a204c218746d02f262060b3b0
divyanshi-bhushan <75475171+divyanshi-bhushan@users.noreply.github.com>
2021-01-27 22:51:51 +0530

    Add files via upload

---
+++ brown.js
@@ -0,0 +1,11 @@
+class Brown extends Block{
+    constructor(x,y,width,height){
+        super(x,y,width,height);
+    }
+
+    display(){
+        fill(102,102,102);
+        super.display();
+    }
+
+}
